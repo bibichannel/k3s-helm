ac4cfc2c5c20496f06ea7ff06ef365698c80ce98
bibichanel <lytrung15901@gmail.com>
2022-06-16 03:35:51 -0400

    update

--- infoCommands.sh
+++ infoCommands.sh
@@ -6,5 +6,7 @@ echo "\n\n***---UNINSTALL K3S---***"
 echo "Uninstall k3s : /usr/local/bin/k3s-agent-uninstall.sh"
 
 echo "\n\n***---INSTALL WORKER NODE WITH COMMANDS---***"
+echo "export K3S_KUBECONFIG_MODE=\"644\""
 echo "export K3S_URL=\"https://$(hostname -I | awk '{print $1}'):6443\""
 echo "export K3S_TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)"
+echo "curl -sfL https://get.k3s.io | sh -"	
