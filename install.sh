86e16a86ce3356fdc19a7e2f763b8161a6cd426b
bibichannel <lytrung15901@gmail.com>
2022-08-26 10:01:04 +0700

    change files

--- install.sh
+++ install.sh
@@ -21,9 +21,30 @@ rm get_helm.sh
 # Install loadbalancer: metallb 
 helm repo add stable https://charts.helm.sh/stable
 helm repo update
+
+export filename=values.yaml
+cat << EOF > $filename
+configInline:
+  address-pools:
+   - name: default
+     protocol: layer2
+     addresses:
+     - 192.168.0.240-192.168.0.250
+EOF
+
 helm upgrade --install --debug --namespace kube-system metallb stable/metallb -f values.yaml
 
+# Install ingress nginx
+helm upgrade --install ingress-nginx ingress-nginx \
+  --repo https://kubernetes.github.io/ingress-nginx \
+  --namespace ingress-nginx --create-namespace --debug
+
+echo "-----------------------------------------------------------"
 echo "=>> 'Control plane' has been installed in the current Node!"
 echo "=>> 'Helm 3' has been installed in the current Node!"
 echo "=>> 'Metallb' has been installed in the current Node!"
+echo "=>> 'Ingress nginx' has been installed in the current Node!"
+echo "-----------------------------------------------------------"
+echo
 echo "Run file 'infoCommand.sh' for the next step!"
+
