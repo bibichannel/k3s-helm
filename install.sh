eadeb8eb78996b8d03e6860ba28c2229965fddef
bibichannel <lytrung15901@gmail.com>
2022-08-26 10:06:54 +0700

    change files

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 set -e
-
-#The first line specifies in which mode we would like to write the k3s configuration (required when not running commands as root)
+# Intall control plane of kubernetes: master node
+echo "-------------> Install Master Node"
 export K3S_KUBECONFIG_MODE="644"
-#the second line actually says k3s not to deploy its default load balancer named servicelb and proxy traefik, instead we will install manually metalb as load balancer and nginx as proxy which are in my opinion better and more widely used.
 export INSTALL_K3S_EXEC=" --no-deploy servicelb --no-deploy traefik"
 
-# Install control plane of kubernetes cluster: k3s
 curl -sfL https://get.k3s.io | sh -
 kubectl config view --raw > ~/.kube/config
 
 # Install package manager of kubernetes: helm 3
 echo "\n\n***---INSTALL HELM  3---***"
+echo "-------------> Install Helm"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 rm get_helm.sh
 
-# Install loadbalancer: metallb 
+# Install loadbalancer: metallb
+echo "-------------> Install Metallb"
 helm repo add stable https://charts.helm.sh/stable
 helm repo update
 
@@ -35,10 +35,12 @@ EOF
 helm upgrade --install --debug --namespace kube-system metallb stable/metallb -f values.yaml
 
 # Install ingress nginx
+echo "-------------> Install Ingress Nginx"
 helm upgrade --install ingress-nginx ingress-nginx \
   --repo https://kubernetes.github.io/ingress-nginx \
   --namespace ingress-nginx --create-namespace --debug
 
+# NOTE
 echo "-----------------------------------------------------------"
 echo "=>> 'Control plane' has been installed in the current Node!"
 echo "=>> 'Helm 3' has been installed in the current Node!"
