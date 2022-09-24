8fd08a8856fe715a5f628626e70c3b687dec436b
bibichannel <65753412+bibichannel@users.noreply.github.com>
2022-09-24 12:27:34 +0700

    Update infoCommands.sh

--- infoCommands.sh
+++ infoCommands.sh
@@ -1,10 +1,9 @@
-echo "***---GET TOKEN MASTER NODE---***"
 echo "\n\n***---UNINSTALL K3S---***"
-
 echo "Uninstall k3s : /usr/local/bin/k3s-agent-uninstall.sh"
+
+echo "***---GET TOKEN MASTER NODE---***"
 echo "Token: sudo cat /var/lib/rancher/k3s/server/node-token" 
 echo $(sudo cat /var/lib/rancher/k3s/server/node-token)
-
 
 echo "\n\n***---INSTALL WORKER NODE WITH COMMANDS---***"
 echo "export K3S_KUBECONFIG_MODE=\"644\""
